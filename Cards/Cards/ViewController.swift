242142d3fb0c79b9cf8e6304ff1f87e9c8495db7
jicaey <young.ma@me.com>
2016-09-06 17:53:50 -0400

    Lab Complete

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -11,9 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Create IB outlets
 
+    @IBOutlet weak var top: UILabel!
+    @IBOutlet weak var bottom: UILabel!
+    @IBOutlet weak var center: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
+    
+    @IBAction func spadeFour(sender: UIButton) {
+        top.text = "♣️"
+        bottom.text = "♣️"
+        center.text = "4"
+    }
+    
+    @IBAction func spadeThree(sender: UIButton) {
+        top.text = "♠️"
+        bottom.text = "♠️"
+        center.text = "3"
+    }
+    
+    @IBAction func diamondEight(sender: UIButton) {
+        top.text = "♦️"
+        bottom.text = "♦️"
+        center.text = "8"
+    }
+    
+    @IBAction func heartTen(sender: UIButton) {
+        top.text = "♥️"
+        bottom.text = "♥️"
+        center.text = "10"
+    }
+    
     // TODO: IB actions and code to update UI
 }
